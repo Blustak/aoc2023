decad5273d3e5bc02fb4786975831759df6c2beb
Blustak <menditta1@gmail.com>
2023-12-04 22:15:37 +0000

    solved p3a, eol error fixed

--- day-3/cpp/problem-1/solution.cpp
+++ day-3/cpp/problem-1/solution.cpp
@@ -17,6 +17,7 @@ struct walkable_dimensions {
 const char skip_char = '.';
 
 std::vector<std::vector<char>> read_file(const std::string &file_name);
+
 int main(int argc, char **argv) {
   std::vector<std::vector<char>> arena = read_file(argv[1]);
   std::vector<int> valid_parts;
@@ -27,96 +28,69 @@ int main(int argc, char **argv) {
 
   // scan for digit
   for (int y = 0; y < y_bottom_bound; y++) {
-    x_right_bound = arena[y].size();
+    x_right_bound = arena[y].size() - 1; //-1 for end of line character perhaps?
     for (int x = 0; x < x_right_bound; x++) {
       if (isdigit(arena[y][x])) {
-        // create the number;
-        int x_offset = 0;
         std::string number;
+        int x_offset = 0;
+        std::vector<char> adj_symbols;
+        bool above = y - 1 > 0;
+        bool below = y + 1 < y_bottom_bound;
+        // add the lefthand symbols
+        if (x - 1 > 0) {
+          adj_symbols.push_back(arena[y][x - 1]);
+          if (above) {
+            adj_symbols.push_back(arena[y - 1][x - 1]);
+          }
+          if (below) {
+            adj_symbols.push_back(arena[y + 1][x - 1]);
+          }
+        }
         while (x + x_offset < x_right_bound &&
                isdigit(arena[y][x + x_offset])) {
           number += arena[y][x + x_offset];
+          if (above) {
+            adj_symbols.push_back(arena[y - 1][x + x_offset]);
+          }
+          if (below) {
+            adj_symbols.push_back(arena[y + 1][x + x_offset]);
+          }
           x_offset++;
         }
-        // check to see if its valid.
-        bool valid_part = false;
-        walkable_dimensions d = {
-            .left = x > x_left_bound,
-            .right = x + x_offset<x_right_bound, .up = y> y_top_bound,
-            .down = y + 1 < y_bottom_bound};
-
-        if (d.up) {
-          for (int i = x; i < x + x_offset; i++) {
-            if (arena[y - 1][i] != skip_char) {
-              std::cout << "Found " << arena[y - 1][i] << "at y:" << y - 1
-                        << ", x:" << i << "\n";
-              valid_part = true;
-            }
+        // add the righthand symbols
+        if (x + x_offset < x_right_bound) {
+          adj_symbols.push_back(arena[y][x + x_offset]);
+          if (above) {
+            adj_symbols.push_back(arena[y - 1][x + x_offset]);
           }
-        }
-        if (d.down) {
-          for (int i = x; i < x + x_offset; i++) {
-            if (arena[y + 1][i] != skip_char) {
-              std::cout << "Found " << arena[y + 1][i] << "at y:" << y + 1
-                        << ", x:" << i << "\n";
-
-              valid_part = true;
-            }
+          if (below) {
+            adj_symbols.push_back(arena[y + 1][x + x_offset]);
           }
-        }
-        if (d.left) {
-          if (d.up) {
-            if (arena[y - 1][x - 1] != skip_char) {
-              std::cout << "Found " << arena[y - 1][x - 1] << "at y:" << y - 1
-                        << ", x:" << x - 1 << "\n";
-              valid_part = true;
-            }
-          }
-          if (d.down) {
-            if (arena[y + 1][x - 1] != skip_char) {
-              std::cout << "Found " << arena[y + 1][x - 1] << "at y:" << y + 1
-                        << ", x:" << x - 1 << "\n";
-              valid_part = true;
-            }
-          }
-          if (arena[y][x - 1] != skip_char) {
-            std::cout << "Found " << arena[y][x - 1] << "at y:" << y
-                      << ", x:" << x - 1 << "\n";
-            valid_part = true;
-          }
-        }
-        if (d.right) {
-          if (d.up) {
-            if (arena[y - 1][x + x_offset] != skip_char) {
-              std::cout << "Found " << arena[y - 1][x + x_offset]
-                        << "at y:" << y - 1 << ", x:" << x + x_offset << "\n";
-              valid_part = true;
-            }
-          }
-          if (d.down) {
-            if (arena[y + 1][x + x_offset] != skip_char) {
-              std::cout << "Found " << arena[y + 1][x + x_offset]
-                        << "at y:" << y + 1 << ", x:" << x + x_offset << "\n";
-              valid_part = true;
-            }
-          }
-          if (arena[y][x + x_offset] != skip_char) {
-            std::cout << "Found " << arena[y][x + x_offset] << "at y:" << y
-                      << ", x:" << x + x_offset << "\n";
-            valid_part = true;
-          }
-        }
-        if (valid_part) {
-          std::cout << "Adding number: " << number << "\n"
-                    << "Found at x:" << x << ", y:" << y << "\n";
-          valid_parts.push_back(std::stoi(number));
         }
         x = x + x_offset;
+
+        // Now do the adding logic
+        for (char c : adj_symbols) {
+          if (c != skip_char) {
+            std::cout << "Number: " << number << "\n";
+            std::cout << "adjacent symbols: ";
+            for (char c : adj_symbols) {
+              std::cout << c << ", ";
+            }
+            std::cout << "\n";
+            valid_parts.push_back(std::stoi(number));
+            break;
+          }
+        }
       }
     }
   }
-  std::cout << "The sum i got was "
-            << std::reduce(valid_parts.begin(), valid_parts.end()) << "\n";
+
+  unsigned int sum = 0;
+  for (int i : valid_parts) {
+    sum += i;
+  }
+  std::cout << "The final sum I got was... " << sum << "!\n";
 }
 
 std::vector<std::vector<char>> read_file(const std::string &file_name) {
